6422663be0bbc2bde42dbbd92cd1c9167c3ed80b
hbybyyang <2451759073@qq.com>
2022-05-22 21:53:03 +0000

    前端

---
+++ src/web/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Hello {{ name }}!</div>
+</template>
+
+<script lang="ts">
+    export default {
+        data() {
+            return {
+                name: 'Vue',
+            }
+        },
+    }
+</script>
