b6009cf25906c474a1f4664bab6fbb770a71d7f6
hbybyyang <2451759073@qq.com>
2022-06-07 10:26:28 +0000

    .

--- start_production.sh
+++ start_production.sh
@@ -1,1 +1,1 @@
-git pull && npm i && npm run build && npm run db:push && npm run build && npm run pm2:start:prod && echo "ok!" && read a
+git pull && npm i && npm run build && npm run db:push:prod && npm run build && npm run pm2:start:prod && echo "ok!" && read a
--- start_release.sh
+++ start_release.sh
@@ -1,1 +1,1 @@
-git pull && npm i && npm run build && npm run db:push && npm run build && npm run pm2:start:re && echo "ok!" && read a
+git pull && npm i && npm run build && npm run db:push:re && npm run build && npm run pm2:start:re && echo "ok!" && read a
