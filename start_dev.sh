ed2a3a59671953eebc0e57edba5844391f628b5e
hbybyyang <2451759073@qq.com>
2022-06-09 13:18:26 +0000

    .

--- start_dev.sh
+++ start_dev.sh
@@ -3,7 +3,7 @@ npm i
 npm run build:all:lazy
 npm run db:push:dev
 npm run pm2:stop:dev
-npm run build
+npm run build:all
 npm run pm2:start:dev
 echo "操作完成, 按回车关闭."
 read a
--- start_re.sh
+++ start_re.sh
@@ -3,7 +3,7 @@ npm i
 npm run build:all:lazy
 npm run db:push:re
 npm run pm2:stop:re
-npm run build
+npm run build:all
 npm run pm2:start:re
 echo "操作完成, 按回车关闭."
 read a
