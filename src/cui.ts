47aa9d6663e556275ee634700cb9ae7b46a0189e
hbybyyang <2451759073@qq.com>
2022-06-15 11:31:22 +0000

    .

--- src/cui.ts
+++ src/cui.ts
@@ -1,4 +1,30 @@
-async function main() {
+import { Kysely, MysqlDialect } from 'kysely'
+import { 获得环境变量 } from '../lib/getEnv'
+import Database from '../tools/types/Database'
+import * as R from 'ramda'
+import L from 'lodash'
+
+async function main(kysely: Kysely<Database>) {
     console.log('hello, world!')
 }
-main()
+async function _main() {
+    var { DB_HOST, DB_PORT, DB_USER, DB_PWD, DB_NAME } = 获得环境变量()
+    var kysely = new Kysely<Database>({
+        dialect: new MysqlDialect({
+            host: DB_HOST,
+            port: DB_PORT,
+            user: DB_USER,
+            password: DB_PWD,
+            database: DB_NAME,
+        }),
+    })
+
+    try {
+        await main(kysely)
+    } catch (e) {
+        console.error('出错了:', e)
+    } finally {
+        await kysely.destroy()
+    }
+}
+_main()
