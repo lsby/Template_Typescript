f677810789405b239b6a8d8318b367c8045d4619
hbybyyang <2451759073@qq.com>
2022-08-06 15:45:19 +0800

    .

--- src/Class/函子.ts
+++ src/Class/函子.ts
@@ -8,9 +8,12 @@ export interface 函子<A> {
 
 // function f<A extends TT, B extends number>(a: A<B>) {}
 
+type MyType<A, B> = [A, B]
+
 type Lambda1转字符串<A> = A extends Lambda1<infer n> ? n : never
 type 一阶类型<A> = A extends 'number' ? number : never
 type 二阶类型<A, B> = A extends 'Array' ? Array<B> : never
+type 三阶类型<A, B, C> = A extends 'MyType' ? MyType<B, C> : never
 
 type Lambda类型翻译<A> = A extends Lambda1<infer n>
   ? 一阶类型<n>
@@ -19,5 +22,5 @@ type Lambda类型翻译<A> = A extends Lambda1<infer n>
   : never
 
 type TT = Lambda2<'A', Lambda2<'B', Lambda3<Lambda3<Lambda1<'MyType'>, Lambda1<'A'>>, Lambda1<'B'>>>>
-type xx = Lambda3<Beta规约<Lambda3<TT, Lambda1<'number'>>>, Lambda1<'string'>>
+type xx = Beta规约<Lambda3<Beta规约<Lambda3<TT, Lambda1<'number'>>>, Lambda1<'string'>>>
 type dd = Lambda类型翻译<xx>
