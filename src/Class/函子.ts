4c30c7e8fab804883d8d3cf4cde4e9ff44875430
hbybyyang <2451759073@qq.com>
2022-08-06 15:33:30 +0800

    tmp

---
+++ src/Class/函子.ts
@@ -0,0 +1,23 @@
+import { Beta规约, Lambda1, Lambda2, Lambda3, Lambda项 } from '@lsby/ts_lambda_type'
+
+export interface 函子<A> {
+  map<B>(f: (a: A) => B): 函子<B>
+}
+
+// type TT<A> = [A]
+
+// function f<A extends TT, B extends number>(a: A<B>) {}
+
+type Lambda1转字符串<A> = A extends Lambda1<infer n> ? n : never
+type 一阶类型<A> = A extends 'number' ? number : never
+type 二阶类型<A, B> = A extends 'Array' ? Array<B> : never
+
+type Lambda类型翻译<A> = A extends Lambda1<infer n>
+  ? 一阶类型<n>
+  : A extends Lambda3<infer a, infer b>
+  ? 二阶类型<Lambda1转字符串<a>, Lambda类型翻译<b>>
+  : never
+
+type TT = Lambda2<'A', Lambda2<'B', Lambda3<Lambda3<Lambda1<'MyType'>, Lambda1<'A'>>, Lambda1<'B'>>>>
+type xx = Lambda3<Beta规约<Lambda3<TT, Lambda1<'number'>>>, Lambda1<'string'>>
+type dd = Lambda类型翻译<xx>
--- src/Model/Maybe.ts
+++ src/Model/Maybe.ts
@@ -1,37 +1,9 @@
-import { FunctorNext, instanceMap, isFunctor } from '../Class/Functor'
+import { 函子 } from '../Class/函子'
 
-// 类型定义
-const 构造子: unique symbol = Symbol('构造子')
-const 参数: unique symbol = Symbol('参数')
-export type Maybe<A> = { [构造子]: 'Just'; [参数]: [A] } | { [构造子]: 'Nothing'; [参数]: [] }
-
-// 构造子
-export function Just<A>(a: A): Maybe<A> {
-  return { [构造子]: 'Just', [参数]: [a] }
-}
-export function Nothing<A>(): Maybe<A> {
-  return { [构造子]: 'Nothing', [参数]: [] }
-}
-
-// 类型类实现
-declare module '../Class/Functor' {
-  interface InstanceFunctor<A> {
-    'Maybe<A>': typeof 构造子 extends keyof A ? (A[typeof 构造子] extends 'Maybe' ? true : false) : false
+export class Maybe<A> implements 函子<A> {
+  constructor(private value: null | A) {}
+  map<B>(f: (a: A) => B): Maybe<B> {
+    if (this.value == null) return this as any
+    return new Maybe(f(this.value))
   }
-  interface map {
-    <A, B>(f: (a: A) => B, a: Maybe<A>): Maybe<B>
-  }
-}
-instanceMap.push(function (f: any, a: any) {
-  if (a[构造子] != 'Maybe') return FunctorNext
-  return map(f, a)
-})
-
-// 函数
-export function pure<A>(a: A) {
-  return Just(a)
-}
-export function map<A, B>(f: (a: A) => B, a: Maybe<A>): Maybe<B> {
-  if (a[构造子] == 'Nothing') return Nothing()
-  return Just(f(a[参数][0]))
 }
