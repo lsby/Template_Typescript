6acf324d2e4529e75951e74f47007c49ced4a9f7
hbybyyang <2451759073@qq.com>
2022-08-18 16:30:33 +0800

    扩充Vue响应值推导定义

--- src/Model/Vue响应值.ts
+++ src/Model/Vue响应值.ts
@@ -10,6 +10,13 @@ export function Vue响应值<A>(数据: A) {
   return { [类型]: 'Vue响应值' as 'Vue响应值', [构造子]: 'Vue响应值' as 'Vue响应值', [参数]: { 数据: ref(数据) } }
 }
 
+// 扩充推导定义
+declare module '@lsby/ts_struct/src/Base/K1' {
+  interface 一阶类型<A1> {
+    DemoPage1: Vue响应值<A1>
+  }
+}
+
 // 函数
 export function 修改值<A>(a: Vue响应值<A>, 新值: A): Effect<null> {
   return Effect(() => {
