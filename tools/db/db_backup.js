aa9566f5bbcd5508f75216809e08b588e5a8a627
hbybyyang <2451759073@qq.com>
2022-08-22 06:59:57 +0800

    .

--- tools/db/db_backup.js
+++ tools/db/db_backup.js
@@ -1,9 +1,12 @@
 var { 获得环境变量 } = require('./lib')
 var mysqldump = require('mysqldump')
 var path = require('path')
+var makeDir = require('make-dir')
 
 async function main() {
   var { DB_HOST, DB_PORT, DB_USER, DB_PWD, DB_NAME } = 获得环境变量()
+
+  await makeDir(path.resolve(__dirname, `../../dump`))
   await mysqldump({
     connection: {
       host: DB_HOST,
