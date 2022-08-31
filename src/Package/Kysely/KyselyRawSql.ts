0ae404b50d2702828c2d1681dcd33287b64b343d
hbybyyang <2451759073@qq.com>
2022-08-31 09:12:10 +0800

    .

--- src/Package/Kysely/KyselyRawSql.ts
+++ src/Package/Kysely/KyselyRawSql.ts
@@ -3,12 +3,12 @@ import { Aff } from '../Aff/Aff'
 import { Debug } from '../Debug/Debug'
 
 export class KyselyRawSql<A> {
-  constructor(private kysely: Kysely<any>, private sql: string) {}
+  constructor(private kysely: Kysely<any>, private sqlStr: string) {}
   执行(): Aff<A[]> {
     return new Aff(async () => {
       var D = new Debug('Package:Data:KyselyRawSql')
-      D.log('运行的sql是 %s', sql).运行()
-      var res = (await sql(this.sql as any).execute(this.kysely)).rows
+      D.log('运行的sql是 %s', this.sqlStr).运行()
+      var res = (await sql(this.sqlStr as any).execute(this.kysely)).rows
       return res as A[]
     })
   }
