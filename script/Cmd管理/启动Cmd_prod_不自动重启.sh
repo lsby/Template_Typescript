3b826144fe2423093e88617a235fc721c549aa51
hbybyyang <2451759073@qq.com>
2022-09-04 02:44:08 +0800

    .

--- script/Cmd管理/启动Cmd_prod_不自动重启.sh
+++ script/Cmd管理/启动Cmd_prod_不自动重启.sh
@@ -2,6 +2,6 @@ cur_dir=`dirname $0`
 source $cur_dir/../Lib/获取项目名称.sh
 
 pm2 del "prod_cmd_noAutoRestart_$ProName"
-cross-env NODE_ENV=prod DEBUG=App:*,Package:* DEBUG_HIDE_DATE=true pm2 start $cur_dir/../../dist/src/Cmd.js --no-autorestart --output $cur_dir/../../logs/out.txt --error $cur_dir/../../logs/error.txt --merge-logs --log-date-format="YYYY-MM-DD HH:mm:ss Z" --name "prod_cmd_noAutoRestart_$ProName"
+cross-env NODE_ENV=prod DEBUG=App:*,Package:* DEBUG_HIDE_DATE=true DEBUG_DEPTH=999 pm2 start $cur_dir/../../dist/src/Cmd.js --no-autorestart --output $cur_dir/../../logs/out.txt --error $cur_dir/../../logs/error.txt --merge-logs --log-date-format="YYYY-MM-DD HH:mm:ss Z" --name "prod_cmd_noAutoRestart_$ProName"
 echo "操作完成, 按回车关闭."
 read a
--- script/Service管理/启动服务器_dev.sh
+++ script/Service管理/启动服务器_dev.sh
@@ -2,6 +2,6 @@ cur_dir=`dirname $0`
 source $cur_dir/../Lib/获取项目名称.sh
 
 pm2 del "dev_service_$ProName"
-cross-env NODE_ENV=dev DEBUG=App:*,Package:* DEBUG_HIDE_DATE=true pm2 start $cur_dir/../../dist/src/Service.js --output $cur_dir/../../logs/out.txt --error $cur_dir/../../logs/error.txt --merge-logs --log-date-format="YYYY-MM-DD HH:mm:ss Z" --name "dev_service_$ProName"
+cross-env NODE_ENV=dev DEBUG=App:*,Package:* DEBUG_HIDE_DATE=true DEBUG_DEPTH=999 pm2 start $cur_dir/../../dist/src/Service.js --output $cur_dir/../../logs/out.txt --error $cur_dir/../../logs/error.txt --merge-logs --log-date-format="YYYY-MM-DD HH:mm:ss Z" --name "dev_service_$ProName"
 echo "操作完成, 按回车关闭."
 read a
