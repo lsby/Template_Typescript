6fc60dd6f6b9962ed620005739f58ad565fd3fa3
hbybyyang <2451759073@qq.com>
2022-09-05 07:15:19 +0800

    .

--- src/Package/Vue/Vue.ts
+++ src/Package/Vue/Vue.ts
@@ -20,34 +20,11 @@ export class Vue {
         VueRouter.createRouter({
           history: VueRouter.createWebHashHistory(),
           routes: this.页面们.map((a) => {
-            var 模板 = a.元素.获得模板()
-            var 参数 = a.元素.获得参数()
-            var 事件 = a.元素.获得事件()
-
-            var 实际参数: any = {}
-            Object.keys(参数).forEach((n) => {
-              实际参数[n] = ref(参数[n])
-            })
-            Object.keys(事件).forEach((n) => {
-              实际参数[n] = (
-                (n) =>
-                async (...args: any[]) => {
-                  var 原始值: any = {}
-                  for (var nx of Object.keys(参数)) {
-                    原始值[nx] = toRaw(实际参数[nx].value)
-                  }
-                  var 返回值 = await 事件[n](原始值, ...args).运行为Promise()
-                  Object.keys(返回值).forEach((nn) => {
-                    实际参数[nn].value = 返回值[nn]
-                  })
-                }
-              )(n)
-            })
-
+            var x = a.元素._预处理().运行()
             return {
               path: a.路径,
-              component: 模板,
-              props: 实际参数,
+              component: x.模板,
+              props: x.实际参数,
             }
           }),
         }),
