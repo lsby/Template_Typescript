2bbb65e5a7da42d71c94c60aded74cab5c695bb5
hbybyyang <2451759073@qq.com>
2022-09-09 11:55:23 +0800

    .

--- src/Cmd.ts
+++ src/Cmd.ts
@@ -1,12 +1,3 @@
-import { AppEnv } from './Model/通用模型/AppEnv'
-import { 创建Kysely } from './Model/通用模型/创建Kysely'
-import { Aff } from './Package/Aff/Aff'
-import { Debug } from './Package/Debug/Debug'
+import app from './Model/简单命令行程序/index'
 
-var app = new AppEnv(({ DB_HOST, DB_PORT, DB_USER, DB_PWD, DB_NAME }) =>
-  Aff.do()
-    .bind('D', () => Aff.pure(new Debug('App:Cmd')))
-    .bind('kysely', () => 创建Kysely({ DB_HOST, DB_PORT, DB_USER, DB_PWD, DB_NAME }))
-    .run((env) => Aff.提升Effect(env.D.log('hello'))),
-).运行()
-app.不带回调运行().运行()
+app.运行()
