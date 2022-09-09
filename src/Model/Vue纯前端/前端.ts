3338c70cebc87d5403b52acedd89ad857c82ec55
hbybyyang <2451759073@qq.com>
2022-09-09 12:51:40 +0800

    .

--- src/Model/Vue纯前端/前端.ts
+++ src/Model/Vue纯前端/前端.ts
@@ -8,6 +8,18 @@ import { MyButton } from './Component/MyButton/Index'
 
 localStorage.debug = '*'
 
+var page1 = new DemoPage(
+  {
+    列表: ['a', 'b', 'c'],
+    按钮组件: new MyButton(),
+  },
+  (old, a) =>
+    new Aff(async () => ({
+      ...old,
+      列表: [...old.列表, a],
+    })),
+)
+
 var page2 = new Flex布局([
   new Flex元素(
     new Flex布局([new Flex元素(new DemoPage2({})), new Flex元素(new DemoPage2({}))], {
@@ -21,25 +33,13 @@ var page2 = new Flex布局([
 
 export default Aff.do()
   .bind('D', () => Aff.pure(new Debug('App:Web')))
-  .bind('按钮', () => Aff.pure(new MyButton()))
-  .bind('p', (env) =>
-    Aff.pure(
-      new DemoPage(
-        { 列表: ['a', 'b', 'c'], 按钮组件: env.按钮 },
-        (old, a) =>
-          new Aff(async () => ({
-            ...old,
-            列表: [...old.列表, a],
-          })),
-      ),
-    ),
-  )
+  .bind('page1', (env) => Aff.pure(page1))
   .bind('page2', (env) => Aff.pure(page2))
   .run((env) =>
     Aff.提升Effect(
       new Vue(
         [
-          { 路径: '/', 元素: env.p },
+          { 路径: '/', 元素: env.page1 },
           { 路径: '/page2', 元素: env.page2 },
         ],
         [],
